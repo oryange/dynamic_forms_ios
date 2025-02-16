567993461499536e54fd36d810be9e3016fa6d5e
oryange <oryangestrifezze@gmail.com>
2025-02-16 11:31:24 -0300

    feat: initial project

---
+++ dynamic_form/dynamic_form/dynamic_formApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_formApp.swift
+//  dynamic_form
+//
+//  Created by Sundari on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct dynamic_formApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dynamic_form/dynamic_formTests/dynamic_formTests.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_formTests.swift
+//  dynamic_formTests
+//
+//  Created by Sundari on 16/02/25.
+//
+
+import Testing
+@testable import dynamic_form
+
+struct dynamic_formTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
